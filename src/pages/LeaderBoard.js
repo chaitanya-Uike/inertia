550ae7820ebcf48c790a806415d3f62b651aa84d
chaitanya-Uike <chaitanyaguike@gmail.com>
2022-04-08 17:39:19 +0530

    leaderboard api call

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -1,47 +1,22 @@
 import React from 'react'
 import TitleHero from '../components/titleHero'
 import './leaderboard.css'
-
-const data = [
-    {
-        position: "1",
-        name: "Aryan Jangid",
-        points: "1492"
-    },
-    {
-        position: "2",
-        name: "Darshan kadam",
-        points: "1332"
-    },
-    {
-        position: "1",
-        name: "Aryan Jangid",
-        points: "1492"
-    },
-    {
-        position: "1",
-        name: "Aryan Jangid",
-        points: "1492"
-    },
-    {
-        position: "1",
-        name: "Aryan Jangid",
-        points: "1492"
-    },
-    {
-        position: "1",
-        name: "Aryan Jangid",
-        points: "1492"
-    },
-    {
-        position: "1",
-        name: "Aryan Jangid",
-        points: "1492"
-    },
-
-]
+import { useState, useEffect } from 'react'
 
 function LeaderBoard() {
+    const [leaderboard, setLeaderboard] = useState([])
+
+    const fetchLeaderboard = async () => {
+        const res = await fetch('https://inertia-api.herokuapp.com/leaderboard')
+        const data = await res.json()
+
+        setLeaderboard(data.players)
+    }
+
+    useEffect(() => {
+        fetchLeaderboard()
+    }, [])
+
     return (
         <>
             <div>
@@ -49,37 +24,24 @@ function LeaderBoard() {
             </div>
             <div className='leaderboard-outer-div'>
                 <div className='leaderboard-div'>
-                    {data.map((card) => {
-                        return (<>
-                            <div className='leaderboard-individual-box'>
-                                <div className='leaderboard-name-div'>
-                                    <h3 className='leaderboard-postition'>#{card.position}</h3>
-                                    <h3>{card.name}</h3>
-                                </div>
-                                <div>
-                                    <h3>{card.points}</h3>
-                                </div>
+                    {leaderboard.map((card, position) => {
+                        return (
+                            <div key={position}>
+                                <div className='leaderboard-individual-box' >
+                                    <div className='leaderboard-name-div'>
+                                        <h3 className='leaderboard-postition'>#{position + 1}</h3>
+                                        <h3>{card.username}</h3>
+                                    </div>
+                                    <div>
+                                        <h3>{card.points}</h3>
+                                    </div>
 
+                                </div>
+                                <div className='leaderboard-line'></div>
                             </div>
-                            <div className='leaderboard-line'></div>
-                        </>)
+                        )
                     })}
 
-                </div>
-                <div className='leaderboard-position-outer-div'>
-                    <h2 className='leader-board-your-postion'>Your Position</h2>
-                    <div className='leaderboard-your-postion-div'>
-                        <div className='leaderboard-individual-box-your-position'>
-                            <div className='leaderboard-name-div'>
-                                <h3 className='leaderboard-postition'>#69</h3>
-                                <h3>Prathis Upadhya</h3>
-                            </div>
-                            <div>
-                                <h3>500</h3>
-                            </div>
-
-                        </div>
-                    </div>
                 </div>
             </div>
 
