05116894a3c635ebcf97f70edff7f2ec085b5570
chaitanya-Uike <chaitanyaguike@gmail.com>
2022-04-10 17:48:33 +0530

    leaderboard dropdown added

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -3,26 +3,34 @@ import TitleHero from '../components/titleHero'
 import './leaderboard.css'
 import { useState, useEffect } from 'react'
 
-
-const data=[
-    {
-        username:'aryan jangid',
-        points:'23'
-    },
-    {
-        username:'aryan jangid',
-        points:'23'
-    },
-    {
-        username:'aryan jangid',
-        points:'23'
-    },
-]
 function LeaderBoard() {
     const [leaderboard, setLeaderboard] = useState([])
+    const [events, setEvents] = useState([])
 
     const fetchLeaderboard = async () => {
-        const res = await fetch('https://inertia-api.herokuapp.com/leaderboard')
+        const res = await fetch('https://inertia-api-v2.herokuapp.com/leaderboard')
+        const data = await res.json()
+
+        setLeaderboard(data.players)
+    }
+
+    const fetchEventsList = async () => {
+        const res = await fetch('https://inertia-api-v2.herokuapp.com/events')
+        const data = await res.json()
+
+        setEvents(data.events)
+    }
+
+    const fetchEventLeaderboard = async (e) => {
+        if (e.target.value === 'All') {
+            fetchLeaderboard()
+            return
+        }
+
+        const eventName = e.target.value
+        const url = `https://inertia-api-v2.herokuapp.com/${eventName}/leaderboard`
+
+        const res = await fetch(url)
         const data = await res.json()
 
         setLeaderboard(data.players)
@@ -30,6 +38,7 @@ function LeaderBoard() {
 
     useEffect(() => {
         fetchLeaderboard()
+        fetchEventsList()
     }, [])
 
     return (
@@ -39,8 +48,14 @@ function LeaderBoard() {
             </div>
             <div className='leaderboard-outer-div'>
                 <h2 className='leaderboard-coming-soon'>Coming Soon...</h2>
+                <select id='workshop' name='workshop' onChange={fetchEventLeaderboard}>
+                    <option value="All">All</option>
+                    {
+                        events.map((event, key) => <option key={key} value={event.name}>{event.name}</option>)
+                    }
+                </select>
                 <div className='leaderboard-div'>
-                    {data.map((card, position) => {
+                    {leaderboard.length === 0 ? <h1>No Players found</h1> : leaderboard.map((card, position) => {
                         return (
                             <div key={position}>
                                 <div className='leaderboard-individual-box' >
