2322e7dcfc575d58848302ea517f09410689fde3
chaitanya-Uike <chaitanyaguike@gmail.com>
2022-04-11 22:10:32 +0530

    username search functionality added

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,10 @@ function About() {
                 <TitleHero title="WHO WE ARE" />
             </div>
             <div className='about-us-description'>
-                <p className='about-us-p'>INERTIA’22, DYPCOE is the annual science and technology festival of Dr. D. Y. Patil College of Engineering in Association with Draic. It provides a platform for the student community to develop and showcase their technical progress. It has grown into Pune's Largest Science and Technology Festival, attracting people from all walks of life, including students, academia, professionals, and the general public. So call your friends, mark your calendars, and gear up for INERTIA’22!!!</p>
+                <p className='about-us-p'>INERTIA’22, DYPCOE is the annual science and technology festival of Dr. D. Y. Patil College of Engineering in Association with DRAIC. It provides a platform for the student community to develop and showcase their technical progress. It has grown into Pune's Largest Science and Technology Festival, attracting people from all walks of life, including students, academia, professionals, and the general public. So call your friends, mark your calendars, and gear up for INERTIA’22!!!</p>
             </div>
             <div>
-                <Mentor/>
+                <Mentor />
             </div>
         </>
 
