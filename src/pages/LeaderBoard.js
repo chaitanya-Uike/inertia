0db2accb8266d0b102487f7ffd3cf98e9f6d3790
aryanjangid <aryanjangid2511@gmail.com>
2022-04-12 07:48:38 +0530

    set leaderboard active

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -1,86 +1,86 @@
 import React from 'react'
 import TitleHero from '../components/titleHero'
-// import Loader from '../components/loader'
+import Loader from '../components/loader'
 import './leaderboard.css'
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function LeaderBoard() {
-    // const [leaderboard, setLeaderboard] = useState([])
-    // const [events, setEvents] = useState([])
-    // const [loading, setLoading] = useState(false)
+    const [leaderboard, setLeaderboard] = useState([])
+    const [events, setEvents] = useState([])
+    const [loading, setLoading] = useState(false)
 
-    // const fetchLeaderboard = async () => {
-    //     try {
-    //         setLoading(true)
-    //         const res = await fetch('https://inertia-api-v2.herokuapp.com/leaderboard')
-    //         const data = await res.json()
+    const fetchLeaderboard = async () => {
+        try {
+            setLoading(true)
+            const res = await fetch('https://inertia-api-v2.herokuapp.com/leaderboard')
+            const data = await res.json()
 
-    //         setLoading(false)
-    //         setLeaderboard(data.players)
-    //     } catch (err) {
-    //         console.log(err);
-    //         setLoading(true)
-    //     }
-    // }
+            setLoading(false)
+            setLeaderboard(data.players)
+        } catch (err) {
+            console.log(err);
+            setLoading(true)
+        }
+    }
 
-    // const fetchEventsList = async () => {
-    //     const res = await fetch('https://inertia-api-v2.herokuapp.com/events')
-    //     const data = await res.json()
+    const fetchEventsList = async () => {
+        const res = await fetch('https://inertia-api-v2.herokuapp.com/events')
+        const data = await res.json()
 
-    //     setEvents(data.events)
-    // }
+        setEvents(data.events)
+    }
 
-    // const fetchEventLeaderboard = async (e) => {
-    //     if (e.target.value === 'All') {
-    //         fetchLeaderboard()
-    //         return
-    //     }
+    const fetchEventLeaderboard = async (e) => {
+        if (e.target.value === 'All') {
+            fetchLeaderboard()
+            return
+        }
 
-    //     try {
-    //         setLoading(true)
-    //         const eventName = e.target.value
-    //         const url = `https://inertia-api-v2.herokuapp.com/${eventName}/leaderboard`
+        try {
+            setLoading(true)
+            const eventName = e.target.value
+            const url = `https://inertia-api-v2.herokuapp.com/${eventName}/leaderboard`
 
-    //         const res = await fetch(url)
-    //         const data = await res.json()
+            const res = await fetch(url)
+            const data = await res.json()
 
-    //         setLoading(false)
-    //         setLeaderboard(data.players)
-    //     } catch (err) {
-    //         setLoading(true)
-    //         console.log(err)
-    //     }
-    // }
+            setLoading(false)
+            setLeaderboard(data.players)
+        } catch (err) {
+            setLoading(true)
+            console.log(err)
+        }
+    }
 
-    // useEffect(() => {
-    //     fetchLeaderboard()
-    //     fetchEventsList()
-    // }, [])
+    useEffect(() => {
+        fetchLeaderboard()
+        fetchEventsList()
+    }, [])
 
 
-    // const searchUsers = async (name) => {
-    //     try {
-    //         setLoading(true)
-    //         const res = await fetch(`https://inertia-api-v2.herokuapp.com/player-search?name=${name}`)
-    //         const data = await res.json()
+    const searchUsers = async (name) => {
+        try {
+            setLoading(true)
+            const res = await fetch(`https://inertia-api-v2.herokuapp.com/player-search?name=${name}`)
+            const data = await res.json()
 
-    //         setLoading(false)
-    //         setLeaderboard(data.players)
-    //     } catch (error) {
-    //         console.log(error);
-    //         setLoading(true)
-    //     }
+            setLoading(false)
+            setLeaderboard(data.players)
+        } catch (error) {
+            console.log(error);
+            setLoading(true)
+        }
 
-    // }
+    }
 
-    // const handleUserInput = (e) => {
-    //     if (e.target.value === "") {
-    //         fetchLeaderboard()
-    //         return
-    //     }
+    const handleUserInput = (e) => {
+        if (e.target.value === "") {
+            fetchLeaderboard()
+            return
+        }
 
-    //     searchUsers(e.target.value)
-    // }
+        searchUsers(e.target.value)
+    }
 
     return (
         <>
@@ -88,23 +88,31 @@ function LeaderBoard() {
                 <TitleHero title="LeaderBoard" />
             </div>
             <div className='leaderboard-outer-div'>
-                <h2 className='leaderboard-coming-soon'>Coming Soon...</h2>
+                {/* <h2 className='leaderboard-coming-soon'>Coming Soon...</h2> */}
 
-                {/* <div className="leaderboard-select-box">
-                    <div className='leaderboard-select-your-text-box'>
-                        <input type="text" className='leaderboard-selection-select' name="input-username" id="input-username" placeholder="Enter name" onChange={handleUserInput} />
+                <div className="leaderboard-select-box">
+                    <div className='left-box'>
+                        <div className='leaderboard-select-your-text-box'>
+                            <h2 className='leaderboard-select-your-event'>Check your events</h2>
+                        </div>
+                        <div className='leaderboard-select-your-text-box'>
+                            <input type="text" className='leaderboard-selection-select' name="input-username" id="input-username" placeholder="Enter name" onChange={handleUserInput} />
+                        </div>
                     </div>
-                    <div className='leaderboard-select-your-text-box'>
-                        <h2 className='leaderboard-select-your-event'>Select your Event</h2>
+                    <div className='right-box'>
+                        <div className='leaderboard-select-your-text-box'>
+                            <h2 className='leaderboard-select-your-event'>Select your Event</h2>
+                        </div>
+                        <div>
+                            <select id='workshop' name='workshop' className='leaderboard-selection-select' onChange={fetchEventLeaderboard}>
+                                <option value="All">All</option>
+                                {
+                                    events.map((event, key) => <option key={key} value={event.name}>{event.name}</option>)
+                                }
+                            </select>
+                        </div>
                     </div>
-                    <div>
-                        <select id='workshop' name='workshop' className='leaderboard-selection-select' onChange={fetchEventLeaderboard}>
-                            <option value="All">All</option>
-                            {
-                                events.map((event, key) => <option key={key} value={event.name}>{event.name}</option>)
-                            }
-                        </select>
-                    </div>
+
 
                 </div>
 
@@ -129,7 +137,7 @@ function LeaderBoard() {
                             }
                             )}</>}
 
-                </div> */}
+                </div>
             </div>
 
         </>
